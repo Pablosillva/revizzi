1ba5b2698157a28df39ca44ff018ec9467ff6d76
Pablo Silva <ptox.cs@hotmail.com>
2023-11-19 23:37:59 -0300

    Primeiro commit
    pagina de index quase pronta, faltando criar as outras paginas

---
+++ processa-agendamento.php
@@ -0,0 +1,26 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Captura os dados do formulário
+    $nome = $_POST["nome"];
+    $telefone = $_POST["telefone"];
+    $data = $_POST["data"];
+    $servico = $_POST["servico"];
+    $mensagem = $_POST["mensagem"];
+
+    // Número do WhatsApp (substitua pelo seu número)
+    $numero_whatsapp = "558388088865";
+
+    // Crie a mensagem do WhatsApp
+    $mensagem_whatsapp = "Olá, sou $nome. Gostaria de agendar um serviço de $servico para $data. Minhas informações adicionais são: $mensagem";
+
+    // Codifique a mensagem para URL
+    $mensagem_whatsapp_codificada = urlencode($mensagem_whatsapp);
+
+    // Crie o link do WhatsApp usando a API oficial
+    $link_whatsapp = "https://wa.me/$numero_whatsapp/?text=$mensagem_whatsapp_codificada";
+
+    // Redirecione para a página de confirmação
+    header("Location: confirmacao-agendamento.php?whatsapp=$link_whatsapp");
+    exit();
+}
+?>
