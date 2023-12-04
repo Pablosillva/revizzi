e83425509e79a340fefb2cb182d8210ae4e048e5
Pablo Silva <ptox.cs@hotmail.com>
2023-12-04 19:24:32 -0300

    update

--- processa-agendamento.php
+++ processa-agendamento.php
@@ -1,26 +1,21 @@
 <?php
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Captura os dados do formulário
     $nome = $_POST["nome"];
     $telefone = $_POST["telefone"];
     $data = $_POST["data"];
     $servico = $_POST["servico"];
     $mensagem = $_POST["mensagem"];
 
-    // Número do WhatsApp (substitua pelo seu número)
     $numero_whatsapp = "558386066418";
 
-    // Crie a mensagem do WhatsApp
     $mensagem_whatsapp = "Olá, sou $nome. Gostaria de agendar um serviço de $servico para $data. Minhas informações adicionais são: $mensagem";
 
-    // Codifique a mensagem para URL
     $mensagem_whatsapp_codificada = urlencode($mensagem_whatsapp);
 
-    // Crie o link do WhatsApp usando a API oficial
     $link_whatsapp = "https://wa.me/$numero_whatsapp/?text=$mensagem_whatsapp_codificada";
 
-    // Redirecione para a página de confirmação
-    header("Location: confirmacao-agendamento.php?whatsapp=$link_whatsapp");
-    exit();
+    header("Location: $link_whatsapp");
+    exit;
 }
 ?>
